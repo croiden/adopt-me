7fa5485534ae53e13bf8a26fe345b5088f7f93fa
Lawrence Croiden Lobo <llobo@planview.com>
2021-05-02 15:47:59 +0530

    Add details route, add custom babel config, add carousel

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,50 @@
 import * as React from "react";
 
-const Details = (props) => {
-    return (
-        <pre>
-            <code>{JSON.stringify(props, null, 4)}</code>
-        </pre>
-    );
-};
+import pet from "@frontendmasters/pet";
+
+import Carousel from "./Carousel.js";
+
+class Details extends React.Component {
+    state = {
+        loading: true,
+    };
+    componentDidMount() {
+        pet.animal(this.props.id).then(({ animal }) => {
+            this.setState({
+                name: animal.name,
+                animal: animal.type,
+                location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+                description: animal.description,
+                media: animal.photos,
+                breed: animal.breeds.primary,
+                loading: false,
+            });
+        }, console.error);
+    }
+    render() {
+        if (this.state.loading) {
+            return <h1>Loading ...</h1>;
+        }
+        const {
+            media,
+            animal,
+            breed,
+            location,
+            description,
+            name,
+        } = this.state;
+        return (
+            <div className="details">
+                <Carousel media={media} />
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Details;
